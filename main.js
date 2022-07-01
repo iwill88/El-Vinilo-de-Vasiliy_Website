dae2cd8d269cc93924db666264a9bb27bb518b88
AlexanderC <alex88_upt@hotmail.com>
2022-06-30 19:28:13 -0500

    primera entrega

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ validarCorreo();
 const cliente1 = new Cliente(nombres,apellidos,correoElectronico);
 console.log(cliente1);
 
-//Funciones de Validacion Registro
+//Funciones de Validacion 
 
 function validarNombres() {
     while (nombres ==""){
@@ -41,6 +41,27 @@ function validarCorreo() {
     }
 }
 
+function validarIngreso() {
+    while ((seleccion1 ===null)||(seleccion1 == "")){
+        alert("Ingrese una opcion valida!");
+        seleccion1 = prompt(salida1);
+    }
+}
+
+function validarIngreso3() {
+    while ((seleccion ===null)||(seleccion == "")){
+        alert("Ingrese una opcion valida!");
+        seleccion = prompt(salida);
+    }
+}
+
+function validarIngreso2() {
+    while ((respuestaBuscador ===null)||(respuestaBuscador == "")){
+        alert("El campo esta vacio!");
+        respuestaBuscador = prompt("Escriba el nombre del vinilo que quiera comprar");
+    }
+}
+
 //Insertamos los datos del Nuevo Cliente al Array baseClientes
 
 const nuevoCliente = new Cliente(nombres,apellidos,correoElectronico);
@@ -48,40 +69,102 @@ baseClientes.push(nuevoCliente);
 console.log(baseClientes);
 
 let seleccion ="";
+let seleccion1 ="";
+let salida1 = "Hola " + nombres + " " + apellidos + "!" + "Elige una de las siguientes opciones: \n\n 1. Ir al buscador (solo podras comprar lo que encuentres en la búsqueda) \n 2. Ir al menu principal"
+function mensajeDescuento () {
+    alert("Promocion especial! \n\nTendrás un descuento del 5% por la compra de 3 vinilos o más!");
+}
+    
+
+//Solicitamos ingresar una opcion de "buscar productos" o "menu principal"
+
+seleccion1 = prompt(salida1);
+validarIngreso();
+if (seleccion1 ==1) {
+    respuestaBuscador = prompt("Escriba el nombre del vinilo que quiera comprar");
+    validarIngreso2();
+    arrayBusqueda = vinilosLista.filter((valor) => valor.nombre.includes(respuestaBuscador));
+    console.log(arrayBusqueda);
+    
+  
+} 
+
 let salida = "Hola " + nombres + " " + apellidos + "!" + " Elige cualquiera de nuestros vinilos:\n\n";
 
-for (let producto of vinilosLista) {
-    salida +=  producto.id + " - Nombre: " + producto.nombre + " - Precio: S/." + producto.precio + "\n";
+if(seleccion1==1){
+    mensajeDescuento();
+    for (let producto of arrayBusqueda) {
+        salida += "Id: " + producto.id + "- Nombre: " + producto.nombre + "- Precio: S/." + producto.precio + "\n";
+    } 
+    salida += "a - Finalizar compra \nb - Cancelar compra";
+    compraViniloBusqueda();
+} else if (seleccion1==2){
+    mensajeDescuento();
+    for (let producto of vinilosLista) {
+        salida +=  "Id: " + producto.id + "- Nombre: " + producto.nombre + "- Precio: S/." + producto.precio + "\n";
+    }
+    salida += "a - Finalizar compra \nb - Cancelar compra";
+    compraVinilo();
 }
 
-salida += "6 - Finalizar compra \n7 - Cancelar compra";
+    
+
+
 
 let total_pagar = 0;
 
-while (seleccion != 6 || 7) {
-    seleccion = prompt(salida);
+//Funcion de compra de vinilos menu principal
 
-    if ((seleccion ===null)||(seleccion == 6)) {
-        
-        break;
-        
+function compraVinilo() {
+    while (seleccion != "a"||"b") {
+        seleccion = prompt(salida);
+        validarIngreso3();
+        if ((seleccion ===null)||(seleccion == "a")) {
+            break;
+        }
+    
+        if (seleccion !="b"){
+            producto_encontrado = vinilosLista.find((valor) => valor.id == seleccion);
+            if (producto_encontrado != 0) {
+                let producto_lista = new Producto(producto_encontrado.id, producto_encontrado.nombre, producto_encontrado.precio);
+                productosSeleccionados.push(producto_lista);
+                console.log("Agregarse al Carrito: " + producto_lista.nombre + " (S/." + producto_lista.precio + ")");
+            }
+        }
+        if (seleccion == "b"){
+            alert("Se ha cancelado la compra");
+            alert("Usted esta saliendo de la pagina");
+            break;
+        }
+            
     }
+}
 
+//Funcion de compra de vinilos por busqueda
 
-    let producto_encontrado = buscarProducto(seleccion);
-
-    if (producto_encontrado != 0) {
-        let producto_lista = new Producto(producto_encontrado.id, producto_encontrado.nombre, producto_encontrado.precio);
-        productosSeleccionados.push(producto_lista);
-        console.log("Agregarse al Carrito: " + producto_lista.nombre + " (S/." + producto_lista.precio + ")");
+function compraViniloBusqueda() {
+    while (seleccion != "a"||"b") {
+        seleccion = prompt(salida);
+        validarIngreso3();
+        if ((seleccion ===null)||(seleccion == "a")) {
+            break;
+        }
+    
+        if (seleccion !="b"){
+            producto_encontrado = arrayBusqueda.find((valor) => valor.id == seleccion);
+            if (producto_encontrado != 0) {
+                let producto_lista = new Producto(producto_encontrado.id, producto_encontrado.nombre, producto_encontrado.precio);
+                productosSeleccionados.push(producto_lista);
+                console.log("Agregarse al Carrito: " + producto_lista.nombre + " (S/." + producto_lista.precio + ")");
+            }
+        }
+        if (seleccion == "b"){
+            alert("Se ha cancelado la compra");
+            alert("Usted esta saliendo de la pagina");
+            break;
+        }
+            
     }
-
-    if (seleccion == 7){
-        alert("Se ha cancelado la compra");
-        alert("Usted esta saliendo de la pagina");
-        break;
-    }
-        
 }
 
 console.log(productosSeleccionados);
@@ -99,7 +182,7 @@ for (let producto of productosSeleccionados) {
 
     // Si la compra es mayor a 3 productos se aplicara un descuento
 
-    if (productosSeleccionados.length > 3) {
+    if (productosSeleccionados.length > 2) {
         producto.aplicarDescuento();
         console.log("Precio con Descuento: S/." + producto.precio);
     }
@@ -117,7 +200,7 @@ for (let producto of productosSeleccionados) {
     mensaje +=  numero_compra + " - Nombre: " + producto.nombre + " - Precio: S/." + producto.precio + "\n";
 }
 
-if (seleccion == 6) {
+if (seleccion == "a") {
     alert("Los productos que ha seleccionado son: \n\n" +  mensaje);
     alert("El Total a Pagar es: S/." + total_pagar);
     alert("Muchas gracias por su compra!")
--- productos.js
+++ productos.js
@@ -1,11 +1,16 @@
 //Defino el Array de Vinilos disponibles en Tienda
 
 const vinilosLista = [
-    {id:1,nombre:'"Led Zeppelin-IV"',precio:150},
-    {id:2,nombre:'"Black Sabbath-Heaven and Hell"',precio:140},
-    {id:3,nombre:'"Gun N Roses-Appetite For Destruction"',precio:160},
-    {id:4,nombre:'"The Beatles-Let it Be"',precio:200},
-    {id:5,nombre:'"Bryan Adams-Reckless"',precio:120},
+    {id:1,nombre:'"Led Zeppelin-III"',precio:160},
+    {id:2,nombre:'"Led Zeppelin-IV"',precio:150},
+    {id:3,nombre:'"Black Sabbath-Heaven and Hell"',precio:140},
+    {id:4,nombre:'"Black Sabbath-Paranoid"',precio:130},
+    {id:5,nombre:'"Gun N Roses-Appetite For Destruction"',precio:160},
+    {id:6,nombre:'"Gun N Roses-Chinese Democracy"',precio:150},
+    {id:7,nombre:'"The Beatles-Let it Be"',precio:200},
+    {id:8,nombre:'"The Beatles-Help"',precio:190},
+    {id:9,nombre:'"Bryan Adams-Reckless"',precio:120},
+    {id:10,nombre:'"Bryan Adams-Cuts Like a Knife"',precio:110},
 ];
 
 //Defino el Array de Productos seleccionados
@@ -34,14 +39,5 @@ class Producto {
     
 }
 
-//Busca en el Array el Producto con el ID enviado por parametro. Devuelve un Objeto con el ID
 
-function buscarProducto(id) {
-    for (let producto of vinilosLista) {
-        if (producto.id == id) {
-            return producto;
-        }
-    }
 
-    return [];
-}
